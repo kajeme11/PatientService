bc8c7fac4ed59cfebb3669bd7545b8b6e9253952
kajeme11 <chenequek@gmail.com>
2025-05-22 18:44:56 -0700

    added patient repo, service, controller, model, dto

---
+++ patient-service/src/main/java/com/pm/patient_service/service/PatientService.java
@@ -0,0 +1,27 @@
+package com.pm.patient_service.service;
+
+import com.pm.patient_service.dto.PatientResponseDTO;
+import com.pm.patient_service.mapper.PatientMapper;
+import com.pm.patient_service.model.Patient;
+import com.pm.patient_service.repository.PatientRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class PatientService {
+
+    private PatientRepository patientRepository;
+
+    public PatientService(PatientRepository patientRepository){
+        this.patientRepository = patientRepository;
+    }
+
+    public List<PatientResponseDTO> getPatients(){
+        List<Patient> patients = patientRepository.findAll();
+//        List<PatientResponseDTO> patientResponseDTOS = patients.stream().map(PatientMapper::createPatientDTO).toList();
+        return patients.stream().map(PatientMapper::createPatientDTO).toList();
+    }
+
+}
