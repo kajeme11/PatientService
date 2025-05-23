60987970b5f812aba33c94cf0f4dab94c5278073
kajeme11 <chenequek@gmail.com>
2025-05-22 22:48:00 -0700

    Patient mapper from PatientRequestDTO to Patient

--- patient-service/src/main/java/com/pm/patient_service/mapper/PatientMapper.java
+++ patient-service/src/main/java/com/pm/patient_service/mapper/PatientMapper.java
@@ -1,7 +1,10 @@
 package com.pm.patient_service.mapper;
 
+import com.pm.patient_service.dto.PatientRequestDTO;
 import com.pm.patient_service.dto.PatientResponseDTO;
 import com.pm.patient_service.model.Patient;
+
+import java.time.LocalDate;
 
 public class PatientMapper {
 
@@ -13,6 +16,16 @@ public class PatientMapper {
         patientResponseDTO.setEmail(patient.getEmail());
         patientResponseDTO.setDateOfBirth(patient.getDateOfBirth().toString());
         return patientResponseDTO;
+    }
+
+    public static Patient createPatient(PatientRequestDTO patientRequestDTO){
+        Patient patient = new Patient();
+        patient.setName(patientRequestDTO.getName());
+        patient.setEmail(patientRequestDTO.getEmail());
+        patient.setAddress(patientRequestDTO.getAddress());
+        patient.setDateOfBirth(LocalDate.parse(patientRequestDTO.getDateOfBirth()));
+        patient.setRegisteredDate(LocalDate.parse(patientRequestDTO.getRegisteredDate()));
+        return patient;
     }
 
 }
--- patient-service/src/main/java/com/pm/patient_service/model/Patient.java
+++ patient-service/src/main/java/com/pm/patient_service/model/Patient.java
@@ -27,7 +27,7 @@ public class Patient {
     private LocalDate dateOfBirth;
 
     @NotNull
-    private String registeredDate;
+    private LocalDate registeredDate;
 
 
     public UUID getId() {
@@ -70,11 +70,11 @@ public class Patient {
         this.dateOfBirth = dateOfBirth;
     }
 
-    public @NotNull String getRegisteredDate() {
+    public @NotNull LocalDate getRegisteredDate() {
         return registeredDate;
     }
 
-    public void setRegisteredDate(@NotNull String registeredDate) {
+    public void setRegisteredDate(@NotNull LocalDate registeredDate) {
         this.registeredDate = registeredDate;
     }
 
