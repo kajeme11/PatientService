104ac345fe430e7b385b5210d1eba968971518bc
kajeme11 <chenequek@gmail.com>
2025-05-26 01:31:02 -0700

    Email already exists check in service, and exception created

--- patient-service/src/main/java/com/pm/patient_service/service/PatientService.java
+++ patient-service/src/main/java/com/pm/patient_service/service/PatientService.java
@@ -2,6 +2,7 @@ package com.pm.patient_service.service;
 
 import com.pm.patient_service.dto.PatientRequestDTO;
 import com.pm.patient_service.dto.PatientResponseDTO;
+import com.pm.patient_service.exception.EmailAlreadyExistsException;
 import com.pm.patient_service.mapper.PatientMapper;
 import com.pm.patient_service.model.Patient;
 import com.pm.patient_service.repository.PatientRepository;
@@ -26,6 +27,9 @@ public class PatientService {
     }
 
     public PatientResponseDTO createPatient(PatientRequestDTO patientRequestDTO){
+        if(patientRepository.existsByEmail(patientRequestDTO.getEmail())){
+            throw new EmailAlreadyExistsException("A patient with " + patientRequestDTO.getEmail() + " this email already exists!");
+        }
         Patient newPatient = patientRepository.save(PatientMapper.createPatient(patientRequestDTO));
         return PatientMapper.createPatientDTO(newPatient);
     }
